7d1e945dc88b4a9ae35a43fb53c48f159904e4aa
Teruki Ito <ito.teruki@gmail.com>
2021-11-07 18:42:14 -0800

    adding code for interactive gauge chart

--- charts.js
+++ charts.js
@@ -133,6 +133,49 @@ function buildCharts(sample) {
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot('bubble', bubbleData, bubbleLayout); 
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+  
+  // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+  var metadata = data.metadata;
+  var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+
+  // 2. Create a variable that holds the first sample in the metadata array.
+  var result = resultArray[0];  
+
+  // 3. Create a variable that holds the washing frequency.
+  var washFreq = parseFloat(result.wfreq);
+
+  // 4. Create the trace for the gauge chart.
+  var gaugeData = [{
+    title:{text: 'Scrubs per Week', font: {size: 15}},
+    domain: {x: [0,1], y:[0,1]},
+    value: washFreq,
+    type: 'indicator',
+    mode: 'gague+number',
+   // gauge: {
+     // axis: { range: [null, 10], tickwidth: 1, tickcolor: 'black' },
+     // bar: {color: 'black'},
+     // bgcolor:'white',
+     // borderwidth: 2,
+     // steps: [
+      //  { range: [0, 2], color: 'red'},
+      //  { range: [2,4], color: 'orange'},
+      //  { range: [4,6], color: 'yellow'},
+      //  { range: [6,8], color: 'green'},
+      //  { range: [8,10], color: 'blue'}
+     // ],
+   // }
+  }];
+  // 5. Create the layout for the gauge chart.
+  var gaugeLayout = {
+    title:{text: 'Belly Button Washing Frequency', font: {size: 24}},
+    width: 400,
+    height: 400,
+   // margin: { t: 40, r: 25, l: 25, b: 15 },
+  };
+
+  // 6. Use Plotly to plot the gauge data and layout.
+  Plotly.newPlot('gauge',gaugeData, gaugeLayout);
+
   });
 }
